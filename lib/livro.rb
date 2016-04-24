3e31647eddeff055ba3a7e65b7636c7369ed168e
CRISTYAN ROSSI <CRISTYANROSSI@GMAIL.com>
2016-04-24 00:41:41 -0300

    3.7 Grandes poderes, grandes responsabilidades

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,25 +1,25 @@
 class Livro
 	# encoding: utf-8
 
-	attr_accessor :preco
+	attr_accessor :valor
 
 	def initialize(autor, isbn = '1',numero_de_paginas, preco)
 		@autor = autor
 	 	@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
-		@preco = preco
+		@valor = valor
 	end
 
 	def  to_s
 		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
 	end
 
-	def preco
-		@preco
+	def valor
+		@valor
 	end
 
-	def preco=(preco)
-		@preco = preco
+	def valor=(valor)
+		@valor = valor
 	end
 
 end
