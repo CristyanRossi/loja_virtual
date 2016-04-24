debc6aa9e5a781a881e70dee1c9233ba17962a16
CRISTYAN ROSSI <CRISTYANROSSI@GMAIL.com>
2016-04-24 00:28:38 -0300

    3.5 Alteração e leitura de atributos

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,14 +1,25 @@
-# encoding: utf-8
-
 class Livro
-	def initialize(autor, isbn = '1',numero_de_paginas)
+	# encoding: utf-8
+
+	attr_accessor :preco
+
+	def initialize(autor, isbn = '1',numero_de_paginas, preco)
 		@autor = autor
 	 	@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
 
 	def  to_s
 		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+	end
+
+	def preco
+		@preco
+	end
+
+	def preco=(preco)
+		@preco = preco
 	end
 
 end
